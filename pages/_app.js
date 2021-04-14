f1ab8fa513e99a64dc827ea51456328e9974a3e7
Shubham Sananse <shubhamsananse9@gmail.com>
2021-04-14 22:30:45 +0530

    chakra , firebase initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  _bg: "#eef0f1",
+  _text: "#08090a",
+  _blue: "#3b49df",
+  _red: "#df3b3b",
+  _green: "#3bdf72",
+  _gray: "#b5bdc4",
+};
+
+const theme = extendTheme({
+  colors,
+  fonts: {
+    body: "Inter",
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme} resetCSS>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
